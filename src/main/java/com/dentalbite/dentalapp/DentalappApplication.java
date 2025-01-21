9e08ade4609a0e1b9dd8907d4cba1f48be4e982e
JordySanchezRamirez20 <jordy_rooney10@hotmail.com>
2025-01-20 22:12:33 -0600

    Generación del proyecto

---
+++ src/main/java/com/dentalbite/dentalapp/DentalappApplication.java
@@ -0,0 +1,13 @@
+package com.dentalbite.dentalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dentalbite/dentalapp/DentalappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentalbite.dentalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
